d45b33bd11b95923823ec23270504392b5d4d21c
zhiheng <hua13@illinois.edu>
2021-04-25 20:31:57 +0800

    small modifications

--- Website-Selenium.py
+++ Website-Selenium.py
@@ -1,13 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import *
+import time
 
 path = "C:\\WebDriver\\bin\\chromedriver.exe"  # this should be the path of your chromedriver
 
 driver = webdriver.Chrome(path)
+driver.get("https://quadcamproxy.admin.illinois.edu/video.html")
 
+js = """
+return document.getElementById('video_image_video').src
+"""
 
-driver.get("https://illinois.edu/about/quadcam.html")
-print(driver.title)
-
+while True:
+    try:
+        last_url = driver.execute_script(js)
+        if driver.execute_script(js) != last_url:
+            current_url = driver.execute_script(js)
+            print(current_url)
+            last_url = current_url
+    except:
+        WebDriverWait(driver, 10)
 
 # driver.quit()
 
